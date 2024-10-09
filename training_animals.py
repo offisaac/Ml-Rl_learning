d396be914c662a130fdcfac7522d8b2bd2f55d8a
offisaac <2508924460@qq.com>
2024-10-09 11:21:29 +0800

    优化器改为Adam 图像数据首先归一化 增加了CNN和FC的个数 改小了batch_size

--- training_animals.py
+++ training_animals.py
@@ -10,9 +10,14 @@ import torch.optim as optim
 import torch.nn as nn
 
 import time
+
+compose_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261])
+])
 to_tensor = transforms.ToTensor()
-train_data=torchvision.datasets.CIFAR10("./data",train=True,download=True,transform=to_tensor)
-test_data=torchvision.datasets.CIFAR10("./data",train=False,download=True,transform=to_tensor)
+train_data = torchvision.datasets.CIFAR10("./data", train=True, download=True, transform=compose_transform)
+test_data = torchvision.datasets.CIFAR10("./data", train=False, download=True, transform=compose_transform)
 print(train_data.data[100][11][11])
 
 # class mydataset(dataset.Dataset):
@@ -54,24 +59,29 @@ class mydataset(dataset.Dataset):
             self.Y = dataset.targets
             self.class_num=len(dataset.class_to_idx)
     def __getitem__(self,index):
-        return torch.tensor(self.X[index],dtype=torch.float32).permute(2,0,1),self.Y_unique_heating_code[index]
-        # return torch.tensor(self.X[index], dtype=torch.float32).permute(2, 0, 1), self.Y[index]#理论上应该使用独热编码 但是pytorch对数学公式做了处理 先得到输出的权重 再softmax得到概率 再取-log从取最大值到取最小值 再根据整数序列选取使用哪个输出概率(如果模型好 真实对应的就是最大的概率就是最小的log(P)输出
+        # return torch.tensor(self.X[index],dtype=torch.float32).permute(2,0,1),self.Y_unique_heating_code[index]
+        return torch.tensor(self.X[index], dtype=torch.float32).permute(2, 0, 1), self.Y[
+            index]  # 理论上应该使用独热编码 但是pytorch对数学公式做了处理 先得到输出的权重 再softmax得到概率 再取-log从取最大值到取最小值 再根据整数序列选取使用哪个输出概率(如果模型好 真实对应的就是最大的概率就是最小的log(P)输出
     def __len__(self):
         return len(self.X)
 
 my_train_data=mydataset(train_data)
 my_test_data=mydataset(test_data)
-my_train_data_loader=torch.utils.data.DataLoader(my_train_data,batch_size=1000,shuffle=True)
-my_test_data_loader=torch.utils.data.DataLoader(my_test_data,batch_size=1000,shuffle=True)
+my_train_data_loader = torch.utils.data.DataLoader(my_train_data, batch_size=400, shuffle=True)
+my_test_data_loader = torch.utils.data.DataLoader(my_test_data, batch_size=400, shuffle=True)
 class CNNClassifier(nn.Module):
     def __init__(self,class_num):
         super().__init__()#!外部的输入不应该放在类名后(作为继承) 而是放在init后
         self.conv1=nn.Conv2d(3,64,3,stride=1,padding=1)#这里padding=1是指每个边都向外扩一个"0"的像素 所以对于行 相当于扩了两个像素
         self.pool=nn.MaxPool2d(2,2)
-        self.conv2=nn.Conv2d(64,32,3,stride=1,padding=1)#实际设计图片的卷积层时 通道输出图层数由filter决定 最后给线性层输入的列向量行数字需要根据卷积后的图片尺寸决定 这个有公式计算
+        self.conv2 = nn.Conv2d(128, 256, 3, stride=1,
+                               padding=1)  # 实际设计图片的卷积层时 通道输出图层数由filter决定 最后给线性层输入的列向量行数字需要根据卷积后的图片尺寸决定 这个有公式计算
+        self.conv3 = nn.Conv2d(256, 128, 3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 64, 3, stride=1, padding=1)
         #卷积操作影响图层个数和图的尺寸 池化只影响图的尺寸 图最终尺寸只和初始图尺寸 卷积的stride padding有关 和通道无关 通道只和图层有关
-        self.func1=nn.Linear(32*8*8,128)#怎么从32通道8*8array对应到32*8*8个列向量 底层不用考虑
-        self.func2=nn.Linear(128,class_num)
+        self.func1 = nn.Linear(64 * 8 * 8, 2)  # 怎么从32通道8*8array对应到32*8*8个列向量 底层不用考虑
+        self.func2 = nn.Linear(128, 128)
+        self.func3 = nn.Linear(128, class_num)
         self.relu=nn.ReLU()
         self.dropout = nn.Dropout(0.4)#Dropout用于训练更新参数时随机的让神经元的输出置0 但实际使用和评估时不做改变 因为CNN参数相对于全连接不太多 所以一般给的这个值不大
         self.sigmoid=nn.Sigmoid()
@@ -80,44 +90,52 @@ class CNNClassifier(nn.Module):
         x = self.pool(x)
         x = self.conv2(x)
         x = self.pool(x)
+        x = self.conv3(x)
+        x = self.pool(x)
+        x = self.conv4(x)
+        x = self.pool(x)
         # x = self.dropout(x)#主要是为了防止过拟合使用 防止模型对某些神经元过于依赖 增加泛化能力(希望部分集训练的参数对全集也适应)
         x = x.view(x.size(0),-1)#x = x.view(-1,32*8*8) 这里-1可以在任意位置 表示该位置由输入和另一个位置求得 这里两式等效 将 x 的形状从 [batch size, 32, 8, 8] 变为 [batch size, 2048]。
         x = self.func1(x)
         x = self.func2(x)
-        #x = self.relu(x) #不用加 softmax在crossentropy中做了
+        x = self.func3(x)
+        #x = self.relu(x) #不用加 使用crossentropy 计算Loss内部会对Outputs先进行softmax再计算
         return x
 model = CNNClassifier(my_train_data.class_num)
 criterion = nn.CrossEntropyLoss()
 # optimizer = optim.SGD(model.parameters(), lr=0.0005, momentum=0)
-optimizer = optim.RMSprop(model.parameters(), lr=0.0005,momentum=0.2)
-epochs=200
-# for epoch in range(epochs):
-#     for X,Y in my_train_data_loader:
-#         outputs = model(X)
-#         loss = criterion(outputs,Y)
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#     if (epoch + 1) % 2 == 0:
-#         print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}')
-#     if (epoch ) % 10 == 0:
-#         torch.save(model.state_dict(), f'model_parameter_set/model_parameter_{epoch}')
-#     if loss.item()<0.001:
-#         break
+# optimizer = optim.RMSprop(model.parameters(), lr=0.0005,momentum=0.2)
+optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08,
+                       weight_decay=0)  # 如果没有二阶beta参数 Adam和RMSprop可以认为同效果
+epochs = 500
+for epoch in range(epochs):
+    for X, Y in my_train_data_loader:
+        outputs = model(X)
+        loss = criterion(outputs, Y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    if (epoch + 1) % 2 == 0:
+        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}')
+    if (epoch) % 10 == 0:
+        torch.save(model.state_dict(), f'model_parameter_set/model_parameter_{epoch}')
+    if loss.item() < 0.001:
+        break
 # 保存模型参数 直接保存到指定地址即可
-# 导入模型餐宿 需要创建模型 导入模型参数 模型进行eval处理
-timestamp = time.strftime("%Y%m%d_%H%M%S")
-torch.save(model.state_dict(), f'model_parameter_set/model_parameter_{timestamp}')
-loaded_model = CNNClassifier(class_num=10)
-loaded_model.load_state_dict(torch.load(f'./model_parameter_set/model_parameter_100',weights_only=True))
-loaded_model.eval()  # 设置为评估模式 用于关闭某些正则化操作
+# 导入模型参数 需要创建模型 导入模型参数 模型进行eval处理
+# timestamp = time.strftime("%Y%m%d_%H%M%S")
+# torch.save(model.state_dict(), f'model_parameter_set/model_parameter_{timestamp}')
+# loaded_model = CNNClassifier(class_num=10)
+# loaded_model.load_state_dict(torch.load(f'./model_parameter_set/model_parameter_100',weights_only=True))
+# loaded_model.eval()  # 设置为评估模式 用于关闭某些正则化操作
 
 count_all=0
 count_right=0
 with torch.no_grad():  # 禁用梯度计算，以减少内存占用
     for data,label in my_test_data_loader:
         count_all+=len(data)#orcount_all+=data.size(0) 张量0维度 也就是batch维度的大小
-        _,predict=torch.max(loaded_model(data),1)
+        _, predict = torch.max(model(data), 1)
+        # _, predict = torch.max(loaded_model(data), 1)
         _,real=torch.max(label,1)
         for i,j in zip(predict,real):
             if i==j:
