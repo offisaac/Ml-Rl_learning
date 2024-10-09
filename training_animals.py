0ac12b03f659eed57468d9a85e991272cc0b67f3
offisaac <2508924460@qq.com>
2024-10-09 11:25:08 +0800

    修改连层间通道数目不对应的报错

--- training_animals.py
+++ training_animals.py
@@ -72,14 +72,14 @@ my_test_data_loader = torch.utils.data.DataLoader(my_test_data, batch_size=400, 
 class CNNClassifier(nn.Module):
     def __init__(self,class_num):
         super().__init__()#!外部的输入不应该放在类名后(作为继承) 而是放在init后
-        self.conv1=nn.Conv2d(3,64,3,stride=1,padding=1)#这里padding=1是指每个边都向外扩一个"0"的像素 所以对于行 相当于扩了两个像素
+        self.conv1 = nn.Conv2d(3, 128, 3, stride=1, padding=1)  # 这里padding=1是指每个边都向外扩一个"0"的像素 所以对于行 相当于扩了两个像素
         self.pool=nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(128, 256, 3, stride=1,
                                padding=1)  # 实际设计图片的卷积层时 通道输出图层数由filter决定 最后给线性层输入的列向量行数字需要根据卷积后的图片尺寸决定 这个有公式计算
         self.conv3 = nn.Conv2d(256, 128, 3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(128, 64, 3, stride=1, padding=1)
         #卷积操作影响图层个数和图的尺寸 池化只影响图的尺寸 图最终尺寸只和初始图尺寸 卷积的stride padding有关 和通道无关 通道只和图层有关
-        self.func1 = nn.Linear(64 * 8 * 8, 2)  # 怎么从32通道8*8array对应到32*8*8个列向量 底层不用考虑
+        self.func1 = nn.Linear(64 * 2 * 2, 128)  # 怎么从32通道8*8array对应到32*8*8个列向量 底层不用考虑
         self.func2 = nn.Linear(128, 128)
         self.func3 = nn.Linear(128, class_num)
         self.relu=nn.ReLU()
@@ -94,7 +94,7 @@ class CNNClassifier(nn.Module):
         x = self.pool(x)
         x = self.conv4(x)
         x = self.pool(x)
-        # x = self.dropout(x)#主要是为了防止过拟合使用 防止模型对某些神经元过于依赖 增加泛化能力(希望部分集训练的参数对全集也适应)
+        x = self.dropout(x)  #主要是为了防止过拟合使用 防止模型对某些神经元过于依赖 增加泛化能力(希望部分集训练的参数对全集也适应)
         x = x.view(x.size(0),-1)#x = x.view(-1,32*8*8) 这里-1可以在任意位置 表示该位置由输入和另一个位置求得 这里两式等效 将 x 的形状从 [batch size, 32, 8, 8] 变为 [batch size, 2048]。
         x = self.func1(x)
         x = self.func2(x)
